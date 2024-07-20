52c08e047f9c4763b9213ec68351a73a7e5f4fcc
charlie-black <boompiccasso@gmail.com>
2024-07-20 12:50:23 +0300

    Refactor:removed unnecessary padding

--- lib/antochanges/loan_list_screen.dart
+++ lib/antochanges/loan_list_screen.dart
@@ -15,11 +15,12 @@ class LoanListScreen extends StatefulWidget {
 
 class _LoanListScreenState extends State<LoanListScreen> {
   final _apiServices = APIService();
+  late Future<DynamicResponse> _loanInfoFuture;
 
   @override
   void initState() {
-    _apiServices.getLoanInfo();
     super.initState();
+    _loanInfoFuture = _apiServices.getLoanInfo();
   }
 
   @override
@@ -29,87 +30,83 @@ class _LoanListScreenState extends State<LoanListScreen> {
         title: const Text('Loan Information'),
       ),
       body: FutureBuilder<DynamicResponse>(
-        future: _apiServices.getLoanInfo(),
+        future: _loanInfoFuture,
         builder:
             (BuildContext context, AsyncSnapshot<DynamicResponse> snapshot) {
           Widget child = Center(
             child: CircularLoadUtil(),
           );
+
           if (snapshot.connectionState == ConnectionState.waiting ||
               snapshot.connectionState == ConnectionState.active) {
             child = Center(child: CircularLoadUtil());
-          } else {
-            if (snapshot.hasData) {
-              List<dynamic>? loans = snapshot.data?.dynamicList;
-              List<Map> items = [];
+          } else if (snapshot.hasData) {
+            List<dynamic>? loans = snapshot.data?.dynamicList;
+            List<Map> items = [];
 
-              if (loans != null) {
-                for (var item in loans) {
-                  items.add(item);
-                }
+            if (loans != null) {
+              for (var item in loans) {
+                items.add(item);
               }
+            }
 
-              child = ListView.separated(
-                padding: const EdgeInsets.symmetric(horizontal: 4),
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  var mapItem = items[index];
-                  mapItem.removeWhere(
-                      (key, value) => key == null || value == null);
+            child = ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                var mapItem = items[index];
+                mapItem
+                    .removeWhere((key, value) => key == null || value == null);
 
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        border: Border.all(color: Colors.black, width: 1.0),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      child: Column(
-                        children: [
-                          ...mapItem.entries.map((entry) {
-                            return Container(
-                              padding: const EdgeInsets.symmetric(vertical: 2),
-                              child: ListTile(
-                                leading: Text(
-                                  "${entry.key}:",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                                trailing: Text(
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.black, width: 1.0),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    child: Column(
+                      children: [
+                        ...mapItem.entries.map((entry) {
+                          return ListTile(
+                            leading: Text(
+                              "${entry.key}:",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                            trailing: Text(
+                              entry.value.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: WidgetUtil.getTextColor(
                                   entry.value.toString(),
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    color: WidgetUtil.getTextColor(
-                                      entry.value.toString(),
-                                      entry.key.toString(),
-                                    ),
-                                  ),
-                                  textAlign: TextAlign.right,
+                                  entry.key.toString(),
                                 ),
                               ),
-                            );
-                          }).toList(),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Pay Loan'),
-                          ),
-                        ],
-                      ),
+                              textAlign: TextAlign.right,
+                            ),
+                          );
+                        }).toList(),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Pay Loan'),
+                        ),
+                      ],
                     ),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
-              );
-            } else {
-              child = Center(
-                child: Text('${snapshot.error}'),
-              );
-            }
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            );
+          } else {
+            child = Center(
+              child: Text('${snapshot.error}'),
+            );
           }
 
           return child;
